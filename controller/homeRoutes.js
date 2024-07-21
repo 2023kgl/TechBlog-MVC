eb58bba9e912376fa713e834c15216f6e7015755
Karina Gonzalez <karinagonzalez@Karinas-MacBook-Pro.local>
2024-07-21 15:09:24 -0600

    setup

---
+++ controller/homeRoutes.js
@@ -0,0 +1,7 @@
+//homeRoutes, is for direct to correct html page
+
+// check if user is logged in, with Auth, before going into database for the data
+// otherwise, re-direct to login page
+
+// if logged in , redirect to home page
+
---
+++ public/js/login.js
@@ -0,0 +1,4 @@
+// handle login form by doing a fetch call to the api and POST method for username and password -- database
+// if failed alert vaguely "login failed" for security purpose
+
+ 
